c50b2a8f0d80134516cb728ff0e454812c6f60b6
vinu@123 <95160603+Vinu-01@users.noreply.github.com>
2022-05-22 21:35:00 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html")
+});
+
+
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
